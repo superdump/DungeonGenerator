38422225adb0a02e5c4ce511a8b1dcb837724094
Robert Swain <robert.swain@gmail.com>
2020-01-02 18:08:39 +0100

    Add basic rooms
    Two constant rooms punched out of the map.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,14 +6,17 @@ public enum METHOD
 {
     BORDER,
     RANDOM,
+    ROOMS_AND_CORRIDORS,
 }
 
 public class MapGenerator : MonoBehaviour
 {
     public METHOD method = METHOD.BORDER;
     public GameObject wallPrefab;
-    public int gridSize = 20;
+    public GameObject floorPrefab;
+    public int gridSize = 200;
     public float worldSize = 20;
+    public int maxRooms = 10;
 
     private GameObject[,] map;
     private GameObject mapParent;
@@ -44,32 +47,56 @@ public class MapGenerator : MonoBehaviour
         int border = gridSize > 5 ? 5 : gridSize >= 2 ? gridSize / 2 - 1 : 0;
         Vector3 offset = new Vector3(-0.5f * worldSize, 0.0f, -0.5f * worldSize);
         float step = worldSize / gridSize;
+        var bitset = new BitArray(gridSize * gridSize);
+
+        switch (method)
+        {
+            case METHOD.ROOMS_AND_CORRIDORS:
+                {
+                    var r1 = new Rect(20, 15, 10, 15);
+                    var r2 = new Rect(35, 15, 10, 15);
+                    ApplyRoomToMap(r1, ref bitset);
+                    ApplyRoomToMap(r2, ref bitset);
+                }
+                break;
+            default:
+                break;
+        }
         for (int z = 0; z < gridSize; z++, offset.z += step)
         {
             offset.x = -0.5f * worldSize;
             for (int x = 0; x < gridSize; x++, offset.x += step)
             {
+                GameObject prefab;
                 switch (method)
                 {
                     case METHOD.BORDER:
-                        if (x < border || x >= gridSize - border || z < border || z >= gridSize - border)
-                        {
-                            var obj = Instantiate(wallPrefab, mapParent.transform.position + offset, Quaternion.identity, mapParent.transform);
-                            obj.transform.localScale = new Vector3(step, 1.0f, step);
-                            map[z, x] = obj;
-                        }
+                        prefab = (x < border || x >= gridSize - border || z < border || z >= gridSize - border) ? wallPrefab : floorPrefab;
                         break;
                     case METHOD.RANDOM:
-                        if (Random.Range(0, 2) == 0)
-                        {
-                            var obj = Instantiate(wallPrefab, mapParent.transform.position + offset, Quaternion.identity, mapParent.transform);
-                            obj.transform.localScale = new Vector3(step, 1.0f, step);
-                            map[z, x] = obj;
-                        }
+                        prefab = Random.Range(0, 2) == 0 ? wallPrefab : floorPrefab;
+                        break;
+                    case METHOD.ROOMS_AND_CORRIDORS:
+                        prefab = bitset.Get(z * gridSize + x) ? floorPrefab : wallPrefab;
                         break;
                     default:
+                        prefab = floorPrefab;
                         break;
                 }
+                var obj = Instantiate(prefab, mapParent.transform.position + offset, Quaternion.identity, mapParent.transform);
+                obj.transform.localScale = new Vector3(step, 1.0f, step);
+                map[z, x] = obj;
+            }
+        }
+    }
+
+    void ApplyRoomToMap(in Rect r, ref BitArray m)
+    {
+        for (int z = (int)r.yMin; z < (int)r.yMax; z++)
+        {
+            for (int x = (int)r.xMin; x < (int)r.xMax; x++)
+            {
+                m.Set(z * gridSize + x, true);
             }
         }
     }
